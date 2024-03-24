c1d535689eec044f4b01e4f8a1150ceab87b7b2d
Michael Kasprzyk <michal0kasprzyk@gmail.com>
2024-03-25 00:54:59 +0100

    add resizing to neovide and correct unused theme in lualine

--- lua/revinder/neovide.lua
+++ lua/revinder/neovide.lua
@@ -1,5 +1,8 @@
 if vim.g.neovide then
   vim.g.neovide_cursor_animation_length = 0
-  vim.o.guifont = 'IosevkaTerm Nerd Font:h11'
+  vim.o.guifont = 'JetBrainsMonoNL Nerd Font:h15'
   vim.g.neovide_transparency = 0.9
+  vim.keymap.set({ 'n', 'v' }, '<C-9>', ':lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>')
+  vim.keymap.set({ 'n', 'v' }, '<C-->', ':lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>')
+  vim.keymap.set({ 'n', 'v' }, '<C-0>', ':lua vim.g.neovide_scale_factor = 1<CR>')
 end
