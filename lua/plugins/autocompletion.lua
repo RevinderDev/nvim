08772522dd87820bbd40b13aecbb7e4b9e35f82a
RevinderDev <michal0kasprzyk@gmail.com>
2024-03-28 13:45:57 +0100

    add better autocompletion

--- lua/plugins/autocompletion.lua
+++ lua/plugins/autocompletion.lua
@@ -8,6 +8,7 @@ return { -- autocompletion
       build = (function()
         -- build step is needed for regex support in snippets
         -- this step is not supported in many windows environments
+        --
         -- remove the below condition to re-enable on windows
         if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
           return
@@ -38,8 +39,8 @@ return { -- autocompletion
     -- see `:help cmp`
     local cmp = require 'cmp'
     local luasnip = require 'luasnip'
+    local lspkind = require 'lspkind'
     luasnip.config.setup {}
-
     cmp.setup {
       snippet = {
         expand = function(args)
@@ -47,7 +48,10 @@ return { -- autocompletion
         end,
       },
       completion = { completeopt = 'menu,menuone,noinsert' },
-
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
       -- for an understanding of why these mappings were
       -- chosen, you will need to read `:help ins-completion`
       --
@@ -57,7 +61,8 @@ return { -- autocompletion
         ['<c-n>'] = cmp.mapping.select_next_item(),
         -- select the [p]revious item
         ['<c-p>'] = cmp.mapping.select_prev_item(),
-
+        ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+        ['<Tab>'] = cmp.mapping.select_next_item(),
         -- scroll the documentation window [b]ack / [f]orward
         ['<c-b>'] = cmp.mapping.scroll_docs(-4),
         ['<c-f>'] = cmp.mapping.scroll_docs(4),
@@ -66,7 +71,10 @@ return { -- autocompletion
         --  this will auto-import if your lsp supports it.
         --  this will expand snippets if the lsp sent a snippet.
         ['<c-y>'] = cmp.mapping.confirm { select = true },
-
+        ['<CR>'] = cmp.mapping.confirm {
+          behavior = cmp.ConfirmBehavior.Insert,
+          select = true,
+        },
         -- manually trigger a completion from nvim-cmp.
         --  generally you don't need this, because nvim-cmp will display
         --  completions whenever it has completion options available.
@@ -94,10 +102,31 @@ return { -- autocompletion
         -- for more advanced luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/l3mon4d3/luasnip?tab=readme-ov-file#keymaps
       },
+      formatting = {
+        fields = { 'abbr', 'kind', 'menu' },
+        format = lspkind.cmp_format {
+          mode = 'symbol_text', -- show only symbol annotations
+          maxwidth = 20, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+          -- can also be a function to dynamically calculate max width such as
+          -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
+          ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+          show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+
+          -- The function below will be called before any actual modifications from lspkind
+          -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+          before = function(entry, vim_item)
+            -- vim_item.menu = string.sub(vim_item.menu, 1, 20)
+            return vim_item
+          end,
+        },
+      },
+
       sources = {
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' },
-        { name = 'path' },
+        { name = 'path' }, -- file paths
+        { name = 'nvim_lsp', keyword_length = 3 }, -- from language server
+        { name = 'nvim_lsp_signature_help' }, -- display function signatures with current parameter emphasized
+        { name = 'nvim_lua', keyword_length = 2 }, -- complete neovim's Lua runtime API such vim.lsp.*
+        { name = 'buffer', keyword_length = 2 }, -- source current buffer
         { name = 'crates' },
       },
     }
