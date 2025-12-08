e3596ea9421469857f6e4730984169207b4f64aa
RevinderDev <michal0kasprzyk@gmail.com>
2025-12-08 13:37:12 +0100

    fix: get rid of c-a bind

--- lua/revinder/keymap.lua
+++ lua/revinder/keymap.lua
@@ -11,6 +11,7 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'go to previous [d]
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'go to next [d]iagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'show diagnostic [e]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'open diagnostic [q]uickfix list' })
+vim.keymap.set({ 'n', 'i', 'v' }, '<C-a>', '<nop>')
 vim.diagnostic.config {
   float = {
     border = 'single',
